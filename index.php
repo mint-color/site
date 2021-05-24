5a3a132e6ebdfa93b2c3e31e1fc0d02c0c851bc6
mint-color <84694983+mint-color@users.noreply.github.com>
2021-05-24 22:02:53 +0900

    site
    site

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>test</title>
+	<link rel="stylesheet" href="css/default.css"/>
+</head>
+<body>
+
+<div class="container">
+	<div class="header">
+		<h1>Color of the Year 2021</h1>
+		<h2>Ultimate Gray & Illuminating</h2>
+		<p>Alright! This year's color is very beautiful!</p>
+		<p>Check out the color of the year.</p>
+		<p><i></i>Hex: #f5df4d RGB: 245,233,77 HSL: 52 89% 63% <i></i>Hex: #939597 RGB: 147,149,151 HSL: 210 2% 58%</p>
+	</div>
+	<div class="four-section">
+		<div class="item"></div>
+		<div class="item"></div>
+		<div class="item"></div>
+		<div class="item"></div>
+		<div class="item"></div>
+	</div>
+	<div class="five-section">
+		<div class="item"></div>
+		<div class="item"></div>
+		<div class="item"></div>
+		<div class="item"></div>
+		<div class="item"></div>
+		<div class="item"></div>
+	</div>
+	<div class="four-section">
+		<div class="item"></div>
+		<div class="item"></div>
+		<div class="item"></div>
+		<div class="item"></div>
+		<div class="item"></div>
+	</div>
+	<div class="five-section">
+		<div class="item"></div>
+		<div class="item"></div>
+		<div class="item"></div>
+		<div class="item"></div>
+		<div class="item"></div>
+		<div class="item"></div>
+	</div>
+	<footer class="footer">
+		<div class="copy">&copy; mintcolor 2021</div>
+	</footer>
+</body>
+</html>
